562db2e86e0ad1c512fc4571e83ae06f9369d97c
Andreolle <andreolle18@gmail.com>
2018-08-11 04:24:01 +0000

    adding to sidebar animations open/close

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,24 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faChevronCircleLeft } from '@fortawesome/free-solid-svg-icons'
 
-const Sidebar = () => (
-  <nav className="sidebar">
-    <div className="sidebar__header">
-      <h2 className="sidebar__header--title">Nome do Perfil</h2>
-      <h3 className="sidebar__header--subtitle">Projetos</h3>
-    </div>
-    <ul className="sidebar__nav">
-        <li>Item 1</li>
-        <li>Item 2</li>
-        <li>Item 3</li>
-    </ul>
-    <div className="sidebar__actionbar">
-      <FontAwesomeIcon icon={faChevronCircleLeft} />
-    </div>
-  </nav>
-  );
+class Sidebar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isOpen: true
+    }
+
+    this.toggleMenu = this.toggleMenu.bind(this);
+  }
+  
+  toggleMenu () {
+    this.setState(prevState => ({
+      isOpen: !prevState.isOpen
+    }))
+  }
+
+  render() {
+    return (
+      <nav className={`sidebar ${(this.state.isOpen ? 'open' : 'close')}`}>
+        <div className="sidebar__header">
+          <div className="sidebar__header--title">
+            <h2 className="orgName">Nome do Perfil</h2>
+            <h3 className="subtitle">Projetos</h3>
+          </div>
+          <div className="sidebar__header--avatar">
+            <img src="https://avatars3.githubusercontent.com/u/19934823?s=200&v=4" alt=""/>
+          </div>
+        </div>
+        <ul className="sidebar__nav">
+            <li>Andreolle de Sousa Dantas 1</li>
+            <li>Pedro de Sousa Dantas</li>
+            <li>Paulo Araujo Dantas</li>
+        </ul>
+        <div className="sidebar__actionbar">
+          <FontAwesomeIcon onClick={this.toggleMenu} className="sidebar__actionbar--toggleButton" icon={faChevronCircleLeft} />
+        </div>
+      </nav>
+    )
+  }
+}
 
 Sidebar.propTypes = {};
 
